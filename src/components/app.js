320dce699742d486f7081ae102d61b731c9257f9
Karli Beckner <karlibeckner@Dialexas-MacBook-Pro-4.local>
2019-08-15 09:14:56 -0500

    initialize repo

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,24 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div id="topNav" className="topNav">
+      <div id="logo" className="logo"> <a href="home.html">KARLI & CHAD</a></div>
+          
+          
+      <div id="rsvp" className="rsvp"></div>
+      
+      <div id="navLinksID" className="navLinks">
+        <a href="faq.html">FAQ</a>
+        <a href="weddingParty.html">WEDDING PARTY</a>
+        <a href="schedule.html">SCHEDULE</a>
+        <a href="regestry.html">REGESTRY</a>		
+        <a href="travel.html">TRAVEL</a>
+  
+    
+      
+      </div>
+    </div>	
+    
     );
   }
 }
